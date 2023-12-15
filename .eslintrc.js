8bc4ed70ccbf11f8d92757cbce121338777524dd
Kirill <reushev.kd@gmail.com>
2023-12-15 19:44:25 +0300

    add eslint, fix all files

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+	env: {
+		browser: true,
+
+		es2021: true,
+	},
+
+	extends: [
+		'plugin:react/recommended',
+
+		'plugin:prettier/recommended',
+
+		'prettier',
+	],
+
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+
+			files: ['.eslintrc.{js,cjs}'],
+
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+
+	parserOptions: {
+		ecmaVersion: 'latest',
+
+		sourceType: 'module',
+	},
+
+	plugins: ['react', 'prettier'],
+
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+
+		'react/jsx-indent': [2, 4],
+
+		'@typescript-eslint/explicit-function-return-type': 'off',
+
+		'@typescript-eslint/prefer-nullish-coalescing': 'off',
+
+		'@typescript-eslint/strict-boolean-expressions': 'off',
+
+		indent: [2, 4],
+
+		'prettier/prettier': [
+			'warn',
+
+			{
+				endOfLine: 'off',
+				// printWidth: 120,
+				// singleQuote: true,
+				// semi: false,
+				// jsxSingleQuote: true,
+			},
+		],
+	},
+}
